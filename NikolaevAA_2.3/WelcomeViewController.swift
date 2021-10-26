52e8d044a23841818443d35c43bb2e12abeb7285
AANikolaev <mjop41@gmail.com>
2021-10-26 15:16:59 +0300

    собран макет

--- NikolaevAA_2.3/WelcomeViewController.swift
+++ NikolaevAA_2.3/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  NikolaevAA_2.3
 //
 //  Created by Anton Nikolaev on 26.10.2021.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
+    
+
 
 
 }
-
