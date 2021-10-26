e854b2f47d2f33bf70de42bed86e64d413c24132
AANikolaev <mjop41@gmail.com>
2021-10-26 18:46:53 +0300

    добавил алерты

--- NikolaevAA_2.3/LoginViewController.swift
+++ NikolaevAA_2.3/LoginViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet var nameTextField: UITextField!
+    
+    
+    @IBOutlet var passwordTextFild: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameTextField.text = ""
+        passwordTextFild.text = ""
     
     }
+    
+    @IBAction func logInButton() {
+        let name = nameTextField.text!
+        let password = passwordTextFild.text!
+        if name != "Name" || password != "Password" {
+            getAlert(title: "Invalid Login or Password",
+                     text: "Please enter correct login and password")
+        }
+    }
+    
 
+    @IBAction func forgotName() {
+        getAlert(title: "Oooops!", text: "Your name is Name")
+    }
+    
+    
+    @IBAction func forgotPassword() {
+        getAlert(title: "Oooops!", text: "Your password is Password")
+    }
+    
+    private func getAlert(title: String, text: String) {
+        let alert = UIAlertController(title: title, message: text, preferredStyle: UIAlertController.Style.alert)
 
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
